b3c6ef13766878b7ccf9b06c4ca8b84eebc1791b
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-12-05 16:44:01 -0500

    started adding user card data.

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
@@ -9,11 +10,28 @@ class App extends React.Component {
     }
   }
 
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/tbbcoach')
+      .then(res => {
+        console.log(res)
+        this.setState({
+          ...this.state,
+          user: res.data
+        });
+      })
+  
+      .catch(err => console.log('error', err));
+  }
+
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>GitHub User Card and Followers</h1>
+          <h2>Username: {this.state.user.login}</h2>
+          <h2>Name: {this.state.user.name}</h2>
           <p>
             Edit <code>src/App.js</code> and save to reload.
         </p>
